09c8d63a4e2d3ca7009e0ead27017803ccc8f133
= <natalbornoz@gmail.com>
2017-12-14 17:00:39 -0300

    aplicacion

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+// SPLASH:
+// $(document.)ready(function)(){
+//   $('.fondo').hide();
+//     setTimeout(function(){
+//         $('.initial').fadeOut(500);
+//         $('.fondo').show();
+//     }, 2000);
+// }
